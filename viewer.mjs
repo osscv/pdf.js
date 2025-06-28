27d2e809db636315b765218b0d6b08681b64eb41
osscv <140930559+osscv@users.noreply.github.com>
2025-06-28 22:29:31 +0800

    Add files via upload

--- viewer.mjs
+++ viewer.mjs
@@ -16235,19 +16235,8 @@ initCom(PDFViewerApplication);
     if (!file) {
       return;
     }
-    const viewerOrigin = URL.parse(window.location)?.origin || "null";
-    if (HOSTED_VIEWER_ORIGINS.has(viewerOrigin)) {
-      return;
-    }
-    const fileOrigin = URL.parse(file, window.location)?.origin;
-    if (fileOrigin === viewerOrigin) {
-      return;
-    }
-    const ex = new Error("file origin does not match viewer's");
-    PDFViewerApplication._documentError("pdfjs-loading-error", {
-      message: ex.message
-    });
-    throw ex;
+    // Allow any domain - bypass origin validation
+    return;
   };
   var onFileInputChange = function (evt) {
     if (this.pdfViewer?.isInPresentationMode) {
